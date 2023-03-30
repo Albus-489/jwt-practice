c70292d3ec4eed9fb0790ad5987d17ce54d7ea5f
Albus-489 <bavrzar@gmail.com>
2023-03-30 13:15:27 +0300

    methods connection

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -1,18 +1,21 @@
 const Router = require("express");
+const userController = require("../controllers/user-controller");
 
 const router = new Router();
 
-router.post("/registration");
-router.post("/login");
+router.post("/registration", userController.registration);
+router.post("/login", userController.login);
 
 // logout and refresh tokens
-router.post("/logout");
+router.post("/logout", userController.logout);
 
 // link that will be sent to the email address
-router.get("/activate/:link");
+router.get("/activate/:link", userController.activate);
 
 // endpoint to get refresh token if previous token was expired
-router.get("/refresh");
+router.get("/refresh", userController.refresh);
 
 // test endpoint
-router.get("/users");
+router.get("/users", userController.getUsers);
+
+module.exports = router;
