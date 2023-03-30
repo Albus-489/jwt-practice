893a24f939c27d3d53a2b2d31184467ccd1749f6
Albus-489 <bavrzar@gmail.com>
2023-03-30 13:16:33 +0300

    user controller methods

---
+++ src/server/controllers/user-controller.js
@@ -0,0 +1,46 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getUsers(req, res, next) {
+    try {
+      res.json(["PRAISE", "THE", "SUN!"]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = new UserController();
