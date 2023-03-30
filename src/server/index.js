ce2d2bccd892e70a74050e77920cdb9be4bcb71e
Albus-489 <bavrzar@gmail.com>
2023-03-30 11:02:30 +0300

    db connection method

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL, () => {
+      useNewUrlParser = true;
+      useUnifiedTopology = true;
+    });
     app.listen(PORT, () => {
       console.log("Server started on http://localhost:" + PORT);
     });
