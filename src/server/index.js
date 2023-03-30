a4cf1b5f936444affbc78532da5db827800d3a77
Albus-489 <bavrzar@gmail.com>
2023-03-30 13:16:25 +0300

    DB connection and router

--- src/server/index.js
+++ src/server/index.js
@@ -3,19 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const router = require("./router/index");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use("/api", router);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, () => {
-      useNewUrlParser = true;
-      useUnifiedTopology = true;
-    });
+    // await mongoose.connect(process.env.DB_URL);
     app.listen(PORT, () => {
       console.log("Server started on http://localhost:" + PORT);
     });
