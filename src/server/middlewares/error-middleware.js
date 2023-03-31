3904f64a1bdb04a3c65a82bf503fe64d1a3ec311
Albus-489 <bavrzar@gmail.com>
2023-03-31 15:06:49 +0300

    error middleware bug fix and format

--- src/server/middlewares/error-middleware.js
+++ src/server/middlewares/error-middleware.js
@@ -1,9 +1,12 @@
 const ApiError = require("../exceptions/api-error");
+
 module.exports = function (err, req, res, next) {
-  console.log("ERROR : ", err);
+  console.log(err);
   if (err instanceof ApiError) {
-    return res.status(err.status).json({ message: err, errors: err.errors });
+    return res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors });
   }
 
-  return res.statur(500).json({ message: "Unexpected error" });
+  return res.status(500).json({ message: "Unexpected error" });
 };
