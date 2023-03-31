e0604d17f443c2ba88ba29cf6f8e08589026b5a0
Albus-489 <bavrzar@gmail.com>
2023-03-31 15:30:01 +0300

    refresh tokens on successful validation

--- src/server/services/user-service.js
+++ src/server/services/user-service.js
@@ -57,6 +57,15 @@ class UserService {
     if (!isPasswordsEquals) {
       throw ApiError.BadRequestError("Invalid password.");
     }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
   }
 }
 
