081660736986cc575493e6b581867451e5cafe2b
Albus-489 <bavrzar@gmail.com>
2023-03-31 16:51:20 +0300

    Auth middleware implementation

--- src/server/router/index.js
+++ src/server/router/index.js
@@ -1,6 +1,7 @@
 const Router = require("express");
 const userController = require("../controllers/user-controller");
 const { body } = require("express-validator");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 const router = new Router();
 
@@ -22,6 +23,6 @@ router.get("/activate/:link", userController.activate);
 router.get("/refresh", userController.refresh);
 
 // test endpoint
-router.get("/users", userController.getAllUsers);
+router.get("/users", authMiddleware, userController.getAllUsers);
 
 module.exports = router;
